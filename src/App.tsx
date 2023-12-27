61c848e25cc58be7e8846be1acf32ad9806e1d68
Bruna Santiago <brunasanthiago@outlook.com>
2023-12-27 16:21:26 -0300

    Style: COmponente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from './components/Header'
 import { TodoForm } from './components/TodoForm'
 import { GlobalStyle } from './style/global'
 
 function App() {
   return (
     <>
+      <Header />
       <TodoForm />
       <GlobalStyle />
     </>
