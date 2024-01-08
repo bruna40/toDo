5ff5b5247c1fb80a3f8828f2ae488d0b96a0b2ab
Bruna Santiago <brunasanthiago@outlook.com>
2024-01-08 20:48:16 -0300

    contador de tarefa

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,13 +1,31 @@
 import { useContext } from 'react'
 import { TodoItem } from '../TodoItem'
-import { ContainerList, ItemList } from './style'
+import { ContainerList, ItemList, TaskInformation } from './style'
 import { ListContext } from '../../contexts/List'
 
 export function TodoList() {
   const { tasks } = useContext(ListContext)
 
+  function tasksCompleted() {
+    return tasks.filter((task) => task.isCompleted).length
+  }
+
+  const teste = tasksCompleted()
+
   return (
     <ContainerList>
+      <TaskInformation>
+        <div>
+          <h6>Tarefas criadas</h6>
+          <p>{tasks.length}</p>
+        </div>
+        <div>
+          <h6>Concluídas</h6>
+          <p>
+            {teste} de {tasks.length}
+          </p>
+        </div>
+      </TaskInformation>
       <ItemList>
         {tasks.map((task) => (
           <TodoItem key={task.id} id={task.id} />
